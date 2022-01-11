16a72b8840f0d8efa5dc345ac58e51d41b84f39a
Daniel Gonçalves <danielopesgl05@gmail.com>
2022-01-11 18:26:38 -0300

    feat: ensure FacebookApi get debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -18,6 +18,8 @@ describe('FacebookApi', () => {
 
   beforeEach(() => {
     sut = new FacebookApi(httpClient, clientId, clientSecret);
+
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' });
   });
 
   it('should get app token', async () => {
@@ -29,6 +31,18 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials',
+      },
+    });
+  });
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token',
       },
     });
   });
